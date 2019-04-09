44c398a08154e11d460684b1a38a6a27778a4e67
jenharper <jennifer@quatrefoil.com>
2019-04-09 12:43:09 -0400

    first commit for my test project

---
+++ Assets/Scripts/MainSceneScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uduino;
+
+public class MainSceneScript : MonoBehaviour {
+
+    private int counter = 0;
+    public UduinoManager m_uduinoController;
+
+    void Awake()
+    {
+        m_uduinoController.OnDataReceived += OnDataReceived; //Create the Delegate
+        m_uduinoController.alwaysRead = true; // This value should be On By Default
+    }
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Bonjour!  MainSceneScript is alive.");
+      //  m_uduinoController.DiscoverPorts();
+    }
+
+    //May want to move this to statemanage
+    void OnDataReceived(string data, Uduino.UduinoDevice deviceName)
+    {
+            Debug.Log(deviceName.lastRead  + " aka " + deviceName.identity + " said " + data);   
+    }
+
+    // Update is called once per frame
+    void Update () {
+        counter++;
+        if (counter > 500)
+        {
+            counter = 0;
+            Debug.Log("MainSceneScript heartbeat");
+        }
+	}
+}
