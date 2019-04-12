4ad1ac1d047677e88f98dc31f5577c349f7a2570
jenharper <jennifer@quatrefoil.com>
2019-04-12 13:57:19 -0400

    added the brush and a rotation script

---
+++ Assets/Scripts/AdjustBrushPosition.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustBrushPosition : MonoBehaviour {
+
+    float angleX = 0;
+
+	// Use this for initialization
+	void Start () {
+      
+        Debug.Log("Bonjour!  My initial position is " + transform.localEulerAngles.x + ", " + transform.localEulerAngles.y + ", " + transform.localEulerAngles.z);
+        angleX = transform.localEulerAngles.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            angleX += 10.0f;
+            transform.Rotate(10, 0, 0, Space.Self);
+            //transform.Rotation(10, 0, 0, Space.Self);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            angleX -= 10.0f;
+            transform.Rotate(-10, 0, 0, Space.Self);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            transform.Rotate(-1*angleX, 0, 0, Space.Self);
+            angleX = 0f;
+            //transform.eulerAngles = new Vector3(angleX, 0, 0);
+
+        }
+    }
+}
