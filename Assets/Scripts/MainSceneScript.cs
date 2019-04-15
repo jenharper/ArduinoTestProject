ab43acafca4917ed9310b40003130787396e23a3
jenharper <jennifer@quatrefoil.com>
2019-04-15 13:59:18 -0400

    moved brush controller to main scene script

--- Assets/Scripts/MainSceneScript.cs
+++ Assets/Scripts/MainSceneScript.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Uduino;
 
-public class MainSceneScript : MonoBehaviour {
+public class MainSceneScript : MonoBehaviour
+{
 
     private int counter = 0;
     public UduinoManager m_uduinoController;
     public float BrushAngle;
-    private int PotentiometerReading;
+    private int PotentiometerReading = -1; //an invalid value
+    private float desiredAngle = 0;
+    private bool requestChange = false;
+
+    private const float AngleOfZeroReading = 90;
+    private const float AngleOf1023Reading = -90;
+
+    public GameObject myBrushObject;
 
     void Awake()
     {
@@ -17,9 +25,10 @@ public class MainSceneScript : MonoBehaviour {
     }
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         Debug.Log("Bonjour!  MainSceneScript is alive.");
-      //  m_uduinoController.DiscoverPorts();
+        //  m_uduinoController.DiscoverPorts();
     }
 
     //May want to move this to statemanage
@@ -36,7 +45,7 @@ public class MainSceneScript : MonoBehaviour {
             if (readingOK && temp >= 0 && temp < 1024)
             {
                 PotentiometerReading = temp;
-               // Debug.Log("Potentiometer value = " + PotentiometerReading);
+                // Debug.Log("Potentiometer value = " + PotentiometerReading);
             }
             else
             {
@@ -46,18 +55,76 @@ public class MainSceneScript : MonoBehaviour {
         }
         else
         {
-            Debug.Log("Message from unknown Arduino" + deviceName.name);
+            Debug.Log("Message from unknown Arduino: " + deviceName.name + " on port " + deviceName.identity + ".");
+
         }
-           
+
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         counter++;
         if (counter > 500)
         {
             counter = 0;
             Debug.Log("MainSceneScript heartbeat");
         }
-	}
+
+
+        if ((myBrushObject != null) && (PotentiometerReading >= 0))
+        {
+            desiredAngle = AngleZeroTo360(GetBrushAngleFromPotentiometerReading());
+            myBrushObject.transform.localEulerAngles = new Vector3(desiredAngle, myBrushObject.transform.localEulerAngles.y, myBrushObject.transform.localEulerAngles.z);
+        }
+
+
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    desiredAngle += 10.0f;
+        //    requestChange = true;
+        //}
+        //if (Input.GetKeyDown(KeyCode.Q))
+        //{
+        //    desiredAngle -= 10.0f;
+        //    requestChange = true;
+        //}
+        //if (Input.GetKeyDown(KeyCode.C))
+        //{
+        //    desiredAngle = 0f;
+        //    requestChange = true;
+        //}
+
+
+        //if (requestChange)
+        //{
+        //    requestChange = false;
+        //    desiredAngle = AngleZeroTo360(desiredAngle);
+
+        //    if (myBrushObject != null)
+        //    {
+        //        myBrushObject.transform.localEulerAngles = new Vector3(desiredAngle, myBrushObject.transform.localEulerAngles.y, myBrushObject.transform.localEulerAngles.z);
+        //    }
+        //}
+
+    }
+
+    private float GetBrushAngleFromPotentiometerReading()
+    {
+        float slope = (AngleOf1023Reading - AngleOfZeroReading) / 1023.0f; //rise/run
+
+        return (slope * PotentiometerReading) + AngleOfZeroReading;
+    }
+
+    private float AngleZeroTo360(float inputAngle)
+    {
+        float temp = inputAngle;
+        while (temp < 0)
+            temp += 360;
+
+        while (temp >= 360)
+            temp -= 360;
+
+        return temp;
+    }
 }
